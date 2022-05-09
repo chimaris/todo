10e00a7026e80d8895c022a5d9583822422cc19c
chimaris <sweetnwa48@gmail.com>
2022-05-09 16:40:15 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
-
-const listElement = document.querySelector('ul');
-let deleteBtn;
+// Selecting and assigning <ul> Element
+const ulElement = document.querySelector('ul');
 
 document.querySelector("#add").addEventListener("click", function(e) {
     e.preventDefault();
-
     const todo = document.querySelector("#todo").value;
-    console.log(todo.length);
     if(!todo){
+        // Alert if the user input nothing
         alert("Please write something.. Is a must 😎😉")
     }else {
+        // Creating of <div>, <i> and <li> elements
         const divElement = document.createElement("div");
         const checkBtn = document.createElement("i");
         const listItem = document.createElement("li");
-        deleteBtn = document.createElement("i");
+        const deleteBtn = document.createElement("i");
 
+        // Setting up attributes for checkBtn and deleteBtn icon
         deleteBtn.setAttribute('class', 'fa fa-trash');
         deleteBtn.setAttribute('aria-hidden', 'true');
         checkBtn.setAttribute('class','fa fa-check-square-o hidden');
         checkBtn.setAttribute('aria-hidden', 'true');
         
+        // Assigning user input to the <li> element
         listItem.textContent = todo;
 
-        listElement.append(divElement);
+        // Appending div element to ul and its child
+        ulElement.append(divElement);
         divElement.append(checkBtn, listItem, deleteBtn);  
     }
-    
-
 })
 
-listElement.addEventListener("click", function(e) {
-    console.log(e);
+// Updating the list if the user is done by click on each of the list
+ulElement.addEventListener("click", function(e) {
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("done");
         e.target.previousSibling.classList.toggle("hidden");
     }
 })
 
-listElement.addEventListener("click", function(e) {
-    console.log(e);
+// Deleting the list by clicking deleteBtn
+ulElement.addEventListener("click", function(e) {
     if(e.target.className === "fa fa-trash"){
         e.target.parentElement.classList.add("hidden");
     }
