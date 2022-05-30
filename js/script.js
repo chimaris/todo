76a158fb6ebff7db17f911cff4d9d46b7f743339
chimaris <sweetnwa48@gmail.com>
2022-05-30 17:26:14 +0100

    Restructured the js code

--- js/script.js
+++ js/script.js
@@ -8,25 +8,36 @@ document.querySelector("#add").addEventListener("click", function(e) {
         // Alert if the user input nothing
         alert("Please write something.. Is a must 😎😉")
     }else {
-        // Creating of <div>, <i> and <li> elements
-        const divElement = document.createElement("div");
-        const checkBtn = document.createElement("i");
-        const listItem = document.createElement("li");
-        const deleteBtn = document.createElement("i");
 
-        // Setting up attributes for checkBtn and deleteBtn icon
-        deleteBtn.setAttribute('class', 'fa fa-trash');
-        deleteBtn.setAttribute('aria-hidden', 'true');
-        checkBtn.setAttribute('class','fa fa-check-square-o hidden');
-        checkBtn.setAttribute('aria-hidden', 'true');
+        const html = `
+            <div>
+                <i class="fa fa-check-square-o hidden" aria-hidden="true"></i><li> ${todo} </li> <i class="fa fa-trash" aria-hidden="true"></i>
+            </div>`
+        ulElement.insertAdjacentHTML("afterbegin", html);
+
+        //---- Instead of creating and appending element i just used template literal and insertAdjacentHtml method --// 
         
-        // Assigning user input to the <li> element
-        listItem.textContent = todo;
+        // // Creating of <div>, <i> and <li> elements
+        // const divElement = document.createElement("div");
+        // const checkBtn = document.createElement("i");
+        // const listItem = document.createElement("li");
+        // const deleteBtn = document.createElement("i");
 
-        // Appending div element to ul and its child
-        ulElement.append(divElement);
-        divElement.append(checkBtn, listItem, deleteBtn);
-        document.querySelector("#todo").value = " ";
+        // // Setting up attributes for checkBtn and deleteBtn icon
+        // deleteBtn.setAttribute('class', 'fa fa-trash');
+        // deleteBtn.setAttribute('aria-hidden', 'true');
+        // checkBtn.setAttribute('class','fa fa-check-square-o hidden');
+        // checkBtn.setAttribute('aria-hidden', 'true');
+        
+        // // Assigning user input to the <li> element
+        // listItem.textContent = todo;
+
+        // // Appending div element to ul and its child
+        // ulElement.append(divElement);
+        // divElement.append(checkBtn, listItem, deleteBtn);
+
+
+        document.querySelector("#todo").value = ""; // resetting the text input
     }
 })
 
@@ -40,8 +51,8 @@ ulElement.addEventListener("click", function(e) {
 
 // Deleting the list by clicking deleteBtn
 ulElement.addEventListener("click", function(e) {
+    console.log(e);
     if(e.target.className === "fa fa-trash"){
         e.target.parentElement.classList.add("hidden");
     }
 })
-
